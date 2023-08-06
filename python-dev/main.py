0b25ad6c810a080037cb21f9dc68ef0932af13d8
Clóvis Balreira Rodrigues <62964663+clovisbalreira@users.noreply.github.com>
2023-08-06 15:31:14 -0300

    06-08-2023

---
+++ python-dev/main.py
@@ -0,0 +1,32 @@
+# frontEnd -> "é o que se vê"
+    #html, css, javascript
+#backend -> a logica de funcionamento por tras do site
+    #python
+# framework -> flask -> crir site 
+# ambiente virtual -> local no seu computador com instalações especificas
+# site com os scripts: https://cdnjs.com/
+# bibliotecas a instalar: 
+    # Flask – pip install flask
+    # Socketio – pip install python-socketio / pip install flask-socketio
+    # Simple Websocket – pip install simple-websocket
+
+from flask import Flask, render_template # estruturas para criar o site
+from flask_socketio import SocketIO, send # estruturas para criar o chat
+
+app = Flask(__name__) # cria o site
+app.config["SECRET"] = "ajuiahfa78fh9f78shfs768fgs7f6" # chave de seguranca, pode ser qualquer coisa, mas escolha algo dificil
+app.config["DEBUG"] = True # para testarmos o código, no final tiramos
+socketio = SocketIO(app, cors_allowed_origins="*") # cria a conexão entre diferentes máquinas que estão no mesmo site
+
+@socketio.on("message") # define que a função abaixo vai ser acionada quando o evento de "message" acontecer
+def gerenciar_mensagens(mensagem):
+    print(f"Mensagem: {mensagem}")
+    send(mensagem, broadcast=True) # envia a mensagem para todo mundo conectado no site
+
+@app.route("/") # cria a página do site
+def home():
+    return render_template("index.html") # essa página vai carregar esse arquivo html que está aqui
+
+if __name__ == "__main__":
+    socketio.run(app, host='192.168.1.76') 
+    #socketio.run(app, host='localhost') # define que o app vai rodar no seu servidor local, ou seja, na internet em que o seu computador tá conectado
